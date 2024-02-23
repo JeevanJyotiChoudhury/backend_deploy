3bc2d8c92a8996333921f22795ca5b874f0f755e
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2024-02-23 11:04:06 +0530

    cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://audio-recorder-app.vercel.app",
+    origin: "*",
   })
 );
 app.use("/api/audios", audioRoutes);
