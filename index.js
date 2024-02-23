1b63602ae5c3d60668a59ca148cae44cc563adba
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2024-02-23 11:11:33 +0530

    cors

--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const { audioRoutes } = require("./routes/audio.route");
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  // intercept OPTIONS method
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use("/api/audios", audioRoutes);
 
-app.listen(process.env.port, async () => {
+app.listen(process.env.PORT || 8080, async () => {
   try {
     await connection;
     console.log("Server running at port 8080");
