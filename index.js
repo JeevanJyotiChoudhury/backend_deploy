49ea053afac8968d0eb6d611a855821908e55352
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2024-02-24 16:00:39 +0530

    cors updated

--- index.js
+++ index.js
@@ -7,17 +7,26 @@ const { audioRoutes } = require("./routes/audio.route");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://audio-recorder-app.vercel.app/"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+const corsOptions = {
+  origin: "*", // Allow requests from any origin
+  methods: ["GET", "POST"], // Allow only GET and POST requests
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow only specific headers
+};
+
+app.use(cors(corsOptions));
+
+
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://audio-recorder-app.vercel.app/"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
 app.use("/api/audios", audioRoutes);
 
