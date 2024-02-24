31e6606705bbedef0f1cb5371f7caab4dba10dba
Jeevan Jyoti Choudhury <jeevanjyoti549@gmail.com>
2024-02-24 15:53:49 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://audio-recorder-app.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
